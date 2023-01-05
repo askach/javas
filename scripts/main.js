67e638a9129aedf439fd441d31969c185334705d
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-01-05 13:07:38 +0100

    answers

--- scripts/main.js
+++ scripts/main.js
@@ -15,10 +15,10 @@ function between(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let correctAnswear = 0;
-let incorrectAnswear = 0;
+let correctAnswer = 0;
+let incorrectAnswer = 0;
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 2; i++) {
   const firstNumber = between(0, 10);
   const secondNumber = between(0, 10);
   const multResult = prompt(
@@ -26,20 +26,22 @@ for (let i = 0; i < 10; i++) {
   );
 
   if (firstNumber * secondNumber === parseInt(multResult)) {
-    // alert("Correct result");
-    correctAnswear = parseInt(correctAnswear) + 1;
+    correctAnswer = parseInt(correctAnswer) + 1;
   } else {
-    // alert("Correct result");
-    incorrectAnswear = parseInt(incorrectAnswear) + 1;
+    incorrectAnswer = parseInt(incorrectAnswer) + 1;
   }
 }
 
-if (correctAnswear >= 5) {
-  alert("Test passed!");
+if (correctAnswer >= 1) {
+  alert(`Test passed! ${correctAnswer} / ${correctAnswer + incorrectAnswer}`);
 } else {
-  alert("Test failed");
+  alert(
+    `Test failed, score below 50%: ${correctAnswer} / ${
+      correctAnswer + incorrectAnswer
+    }`
+  );
 }
 
-console.log(correctAnswear);
-console.log(incorrectAnswear);
-console.log(correctAnswear / (correctAnswear + incorrectAnswear));
+console.log(correctAnswer);
+console.log(incorrectAnswer);
+console.log(correctAnswer / (correctAnswer + incorrectAnswer));
